aa0962df51ff65cd5c9d3c88fb17859d76bfd1af
Weiyao Ke <wk42@phy.duke.edu>
2018-02-17 18:12:49 -0500

    typedef double and size_t vector

--- src/TableBase.cpp
+++ src/TableBase.cpp
@@ -1,17 +1,20 @@
 #include "TableBase.h"
 #include <iostream>
 
+Dvec calculate_steps(Dvec low, Dvec high, Dvec)
+
 template <typename T, size_t N>
-TableBase<T, N>::TableBase(std::string Name, T (*f)(std::vector<double>), std::vector<size_t> shape):
-_Name(Name), _rank(N), _power_rank(std::pow(2, _rank)), _shape(shape), 
+TableBase<T, N>::TableBase(std::string Name, T (*f)(Dvec), Svec shape, Dvec low, Dvec high):
+_Name(Name), _rank(N), _power_rank(std::pow(2, _rank)), 
+_shape(shape), _low(low), _high(high), _step(calculate_steps)
 _table(_shape), _approximating_function(f) {
 	std::cout<<_Name << " dim=" << _rank <<std::endl;
 }
 
 template <typename T, size_t N>
-T TableBase<T, N>::InterpolateTable(std::vector<double> values){
-   std::vector<size_t> index(_rank);
-   std::vector<double> w = values;
+T TableBase<T, N>::InterpolateTable(Dvec values){
+   Svec index(_rank);
+   Dvec w = values;
    T result{0};
    for(auto i=0; i<_power_rank; ++i) {
         auto W = 1.0;
@@ -26,7 +29,7 @@ T TableBase<T, N>::InterpolateTable(std::vector<double> values){
 }
 
 template <typename T, size_t N>
-void TableBase<T, N>::SetTableValue(std::vector<size_t> index, T v){
+void TableBase<T, N>::SetTableValue(Svec index, T v){
     _table(index) = v;
 }
 
--- src/TableBase.h
+++ src/TableBase.h
@@ -11,6 +11,8 @@
 #include <type_traits>
 #include "utility.h"
 
+typedef std::vector<double> Dvec;
+typedef std::vector<size_t> Svec;
 
 // Base class of a table of type T with dimension N
 template <typename T, size_t N>
@@ -18,13 +20,14 @@ class TableBase{
 protected:
     const std::string _Name;
     const size_t _rank, _power_rank;
-    const std::vector<size_t> _shape;
+    const Svec _shape;
+    const Dvec _low, _high, _step;
     boost::multi_array<T, N> _table;
-    T (*_approximating_function)(std::vector<double> inputs);
+    T (*_approximating_function)(Dvec inputs);
 public:
-	TableBase(std::string, T (*f)(std::vector<double>), std::vector<size_t>);
-	T InterpolateTable(std::vector<double> values);
-    void SetTableValue(std::vector<size_t> index, T v);
+	TableBase(std::string, T (*f)(Dvec), Svec);
+	T InterpolateTable(Dvec values);
+    void SetTableValue(Svec index, T v);
     void echo(void);
 };
 
