a1ea1b4a84cba3f4c9924f0c3d004393c205a877
Weiyao Ke <wk42@phy.duke.edu>
2019-04-29 15:14:20 -0400

    in/outside bug

--- src/workflow.cpp
+++ src/workflow.cpp
@@ -450,7 +450,7 @@ int update_particle_momentum_Lido(
 
 			// If t-t0 > tauf, or it reaches the boundary of QGP
 			// We check whether it sould be formed
-                        bool outside = temp < 0.16;
+            bool outside = temp < 0.16;
 			if (it->x.t() - it->x0.t() > taun || outside){ 
 				double Acceptance = 0.;
 				if (!it->is_vac){ 
@@ -464,8 +464,8 @@ int update_particle_momentum_Lido(
 					double log_factor = std::sqrt(std::log(1+taun/it->mfp0)
 												 /std::log(1+mean_s/mD2) ) ;
 					double LPM;
-                                        if (outside) LPM = it->mfp0 / taun * log_factor;
-                                        else LPM = it->mfp0 / (it->x.t() - it->x0.t()) * log_factor;
+                    if (outside) LPM = it->mfp0 / (it->x.t() - it->x0.t()) * log_factor;
+                    else LPM = it->mfp0 / taun * log_factor;
 					double DeadCone = std::pow(theta2/(theta2+thetaM2), 2);
 					double Running = alpha_s(kt2n, it->T0)/alpha_s(kt20, it->T0);
 					Acceptance = LPM * Running * DeadCone;
@@ -477,7 +477,7 @@ int update_particle_momentum_Lido(
 						double kt20 = measure_perp(pIn.p0, it->p0).pabs2();
 						double kt2n = measure_perp(pIn.p, it->p).pabs2();
 						if (kt2n > Rvac*kt20) Acceptance = 0.0;
-    	                                        else Acceptance = 1.0;
+    	                else Acceptance = 1.0;
 					}
 				}				
 
